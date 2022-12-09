ebb65cd41f695cf32dfc2a44b236bfc17b329418
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2022-12-09 18:34:09 +0300

    Completed assignmet

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+    let re = /[\W_]/g;
+    let lowRegStr = word.toLowerCase().replace(re, '');
+    let reverseStr = lowRegStr.split('').reverse().join(''); 
+    return reverseStr === lowRegStr;
+  }
 
 /* 
   Add your pseudocode here
